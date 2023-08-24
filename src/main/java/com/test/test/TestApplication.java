a9050f792312ea27a31dd36ae08910238bc17c18
hollmanPinto <hollman.pinto07@gmail.com>
2023-08-24 17:36:54 -0500

    mensaje

--- src/main/java/com/test/test/TestApplication.java
+++ src/main/java/com/test/test/TestApplication.java
@@ -1,5 +1,6 @@
 package com.test.test;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
@@ -7,7 +8,10 @@ import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication()
 public class TestApplication {
+	@Value("${spring.datasource.url}")
+	private static String databaseUrl;
 	public static void main(String[] args) {
 		SpringApplication.run(TestApplication.class, args);
+		System.out.println("URL de la base de datos: " + databaseUrl);
 	}
 }
